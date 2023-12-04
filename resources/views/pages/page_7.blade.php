856c4db14b2d698e4d0040f6ca9a553d82a08326
TranDuyHanh194553 <[200~hanh.tranduy2001@gmail.com>
2023-12-04 15:27:37 +0700

    04_12_15h30

---
+++ resources/views/pages/page_7.blade.php
@@ -0,0 +1,159 @@
+@extends('page_7_layout')
+@section('content')
+<div class="features_items"><!--features_items-->
+						<h2 class="title text-center">Đề xuất</h2>
+						<div class="col-sm-2">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+										<div class="productinfo text-left">
+											<img src="public/frontend/images/product1.jpg" alt="" />
+											<!-- <h2>$56</h2> -->
+											<p>Tên: </p>
+											<p>Tuổi: </p>
+											<p>Địa chỉ: </p>
+											<p>Quan tâm: </p>
+											
+											<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+										</div>
+										<div class="product-overlay">
+											<div class="overlay-content">
+												<h2>Have a date?!</h2>
+												<!-- <p>Easy Polo Black Edition</p> -->
+												<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+											</div>
+										</div>
+								</div>
+								<div class="choose">
+									<ul class="nav nav-pills nav-justified">
+										<li><a href="#"><i class="fa fa-plus-square"></i>🤍</a></li>
+										<li><a href="#"><i class="fa fa-plus-square"></i>💬</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>			
+						
+						<!-- 2 -->
+						<div class="col-sm-2">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+										<div class="productinfo text-left">
+											<img src="public/frontend/images/product1.jpg" alt="" />
+											<!-- <h2>$56</h2> -->
+											<p>Tên: </p>
+											<p>Tuổi: </p>
+											<p>Địa chỉ: </p>
+											<p>Quan tâm: </p>
+											
+											<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+										</div>
+										<div class="product-overlay">
+											<div class="overlay-content">
+												<h2>Have a date?!</h2>
+												<!-- <p>Easy Polo Black Edition</p> -->
+												<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+											</div>
+										</div>
+								</div>
+								<div class="choose">
+									<ul class="nav nav-pills nav-justified">
+										<li><a href="#"><i class="fa fa-plus-square"></i>🤍</a></li>
+										<li><a href="#"><i class="fa fa-plus-square"></i>💬</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>			
+
+						<!-- 3 -->
+						<div class="col-sm-2">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+										<div class="productinfo text-left">
+											<img src="public/frontend/images/product1.jpg" alt="" />
+											<!-- <h2>$56</h2> -->
+											<p>Tên: </p>
+											<p>Tuổi: </p>
+											<p>Địa chỉ: </p>
+											<p>Quan tâm: </p>
+											
+											<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+										</div>
+										<div class="product-overlay">
+											<div class="overlay-content">
+												<h2>Have a date?!</h2>
+												<!-- <p>Easy Polo Black Edition</p> -->
+												<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+											</div>
+										</div>
+								</div>
+								<div class="choose">
+									<ul class="nav nav-pills nav-justified">
+										<li><a href="#"><i class="fa fa-plus-square"></i>🤍</a></li>
+										<li><a href="#"><i class="fa fa-plus-square"></i>💬</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>			
+
+						<!-- 4 -->
+						<div class="col-sm-2">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+										<div class="productinfo text-left">
+											<img src="public/frontend/images/product1.jpg" alt="" />
+											<!-- <h2>$56</h2> -->
+											<p>Tên: </p>
+											<p>Tuổi: </p>
+											<p>Địa chỉ: </p>
+											<p>Quan tâm: </p>
+											
+											<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+										</div>
+										<div class="product-overlay">
+											<div class="overlay-content">
+												<h2>Have a date?!</h2>
+												<!-- <p>Easy Polo Black Edition</p> -->
+												<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+											</div>
+										</div>
+								</div>
+								<div class="choose">
+									<ul class="nav nav-pills nav-justified">
+										<li><a href="#"><i class="fa fa-plus-square"></i>🤍</a></li>
+										<li><a href="#"><i class="fa fa-plus-square"></i>💬</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>			
+
+						<!-- 5 -->
+						<div class="col-sm-2">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+										<div class="productinfo text-left">
+											<img src="public/frontend/images/product1.jpg" alt="" />
+											<!-- <h2>$56</h2> -->
+											<p>Tên: </p>
+											<p>Tuổi: </p>
+											<p>Địa chỉ: </p>
+											<p>Quan tâm: </p>
+											
+											<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+										</div>
+										<div class="product-overlay">
+											<div class="overlay-content">
+												<h2>Have a date?!</h2>
+												<!-- <p>Easy Polo Black Edition</p> -->
+												<!-- <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> -->
+											</div>
+										</div>
+								</div>
+								<div class="choose">
+									<ul class="nav nav-pills nav-justified">
+										<li><a href="#"><i class="fa fa-plus-square"></i>🤍</a></li>
+										<li><a href="#"><i class="fa fa-plus-square"></i>💬</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>			
+					</div><!--features_items-->
+@endsection                    
